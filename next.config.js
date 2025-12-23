ad288b91b73608244bd3b4485f5fd762595b9496
EAbdelilah <87987233+EAbdelilah@users.noreply.github.com>
2025-12-23 17:18:42 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	experimental: {
-		appDir: true,
-	},
-	images: {
-		dangerouslyAllowSVG: true,
-		contentDispositionType: "attachment",
-		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-	},
-};
 
-module.exports = nextConfig;
